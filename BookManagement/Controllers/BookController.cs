49bc4b586e2ce84a57a533f41b6b25d5562e6414
manhhung1011 <domanhhungg1011@gmail.com>
2026-05-29 23:08:08 +0700

    Add project files.

---
+++ BookManagement/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using BookManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        private static List<Book> books = new List<Book>
+    {
+        new Book { Id=1, Name="Clean Code", Price=20 },
+        new Book { Id=2, Name="ASP.NET MVC", Price=15 },
+        new Book { Id=3, Name="Design Pattern", Price=25 }
+    };
+
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+        public IActionResult Detail(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null) return NotFound();
+            return View(book);
+        }
+        // GET: hiển thị form
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: xử lý khi submit
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(book);
+            }
+
+            book.Id = books.Count + 1;
+            books.Add(book);
+            TempData["Message"] = "Thêm sách thành công!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookManagement/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookManagement.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên không được để trống")]
+        public string Name { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0")]
+        public decimal Price { get; set; }
+
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
